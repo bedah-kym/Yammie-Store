d6ff944ef639e3fbccb8de1dee1f6983c8549c3d
BEDAH <>
2023-02-10 09:32:25 +0300

    homepage ui fix not much mpesa fixing

--- YammieStore/settings.py
+++ YammieStore/settings.py
@@ -134,12 +134,15 @@ WSGI_APPLICATION = 'YammieStore.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'mysoko',
+       'USER': 'postgres',
+       'PASSWORD': 'admin',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
